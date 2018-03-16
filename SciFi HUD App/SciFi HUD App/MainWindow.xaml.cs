5400fbe9dd92a3f4364273fd8be3771f57a98917
DaniDPX <danidpx@poweredfox.com>
2018-03-15 15:58:50 -0900

    CPU and RAM Usage is set!

--- SciFi HUD App/SciFi HUD App/MainWindow.xaml.cs
+++ SciFi HUD App/SciFi HUD App/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SciFi_HUD_App
 {
@@ -21,9 +22,36 @@ namespace SciFi_HUD_App
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer Timer = new DispatcherTimer();
+
+        public PerformanceCounter CPU = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        public PerformanceCounter RAM = new PerformanceCounter("Memory", "Available MBytes", null);
+        
+        
+        public Int32 cpuCounter, ramCounter, totalRam = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Timer.Tick += Timer_Tick;
+            Timer.Interval = new TimeSpan(0, 0, 0, 0, 1024);
+            Timer.IsEnabled = true; 
+        }
+
+        static ulong GetTotalRAM()
+        {
+           return new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory/1024/1024;
+        }
+
+        public void Timer_Tick(System.Object sender, System.EventArgs e)
+
+    {
+            cpuCounter = Convert.ToInt32(CPU.NextValue());
+            CPU_Usage.Content = cpuCounter.ToString() + "%";
+
+            ramCounter = Convert.ToInt32(GetTotalRAM()) - Convert.ToInt32(RAM.NextValue());
+            RAM_Usage.Content = ramCounter.ToString() + "Mb";
         }
 
         private void drag_view(object sender, MouseButtonEventArgs e)
@@ -34,8 +62,13 @@ namespace SciFi_HUD_App
         private void OpenTerminal(object sender, RoutedEventArgs e)
         {
             ProcessStartInfo info = new ProcessStartInfo("cmd.exe");
-            info.Arguments = "/C";
             Process.Start(info);
+        }
+
+
+        private void WinLoad(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
